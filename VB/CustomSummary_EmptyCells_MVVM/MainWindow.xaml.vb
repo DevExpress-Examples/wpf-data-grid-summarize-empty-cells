afc7082270fe48ae4b80b6f6dc71f00c79fb7df4
DevExpressExampleBot <fakeEmail>
2023-03-22 19:26:26 +0400

    Source auto update [skip ci]

--- VB/CustomSummary_EmptyCells_MVVM/MainWindow.xaml.vb
+++ VB/CustomSummary_EmptyCells_MVVM/MainWindow.xaml.vb
@@ -1,25 +1,13 @@
-﻿Imports DevExpress.Mvvm
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace CustomSummary_EmptyCells_MVVM
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
